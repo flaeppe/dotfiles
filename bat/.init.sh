ed3eedc86569847a9a419b1446b5dd6d23cbc02f
Petter Friberg <petter_friberg@hotmail.com>
2019-07-13 18:36:36 +0200

    Add custom bat theme and remove init script

--- bat/.init.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env fish
-
-echo "Download theme OneHalfDark"
-curl https://raw.githubusercontent.com/sonph/onehalf/master/sublimetext/OneHalfDark.tmTheme --create-dirs -sLo ~/.config/bat/themes/OneHalfDark.tmTheme
