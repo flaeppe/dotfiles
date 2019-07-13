7c0620fe117fbda571e1694255ec12204ebb2b8c
Petter Friberg <petter_friberg@hotmail.com>
2019-07-13 15:17:48 +0200

    Download bat theme OneHalfDark when installing bat

---
+++ bat/.init.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env fish
+
+echo "Download theme OneHalfDark"
+curl https://raw.githubusercontent.com/sonph/onehalf/master/sublimetext/OneHalfDark.tmTheme --create-dirs -sLo ~/.config/bat/themes/OneHalfDark.tmTheme
