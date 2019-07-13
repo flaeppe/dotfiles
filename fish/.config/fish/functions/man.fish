4729bd19d7965108d7d37bf1ffdbb7996a2071db
Petter Friberg <petter_friberg@hotmail.com>
2019-07-13 17:17:27 +0200

    Implement custom fish man command
    - Mainly to colorise man output

---
+++ fish/.config/fish/functions/man.fish
@@ -0,0 +1,14 @@
+function man --description "colorise man pages with a wrapper"
+    set -x LESS_TERMCAP_mb (set_color green)  # Begin blinking 
+    set -x LESS_TERMCAP_md (set_color --bold green)  # Start of bold
+    set -x LESS_TERMCAP_me (set_color normal)  # End of all formatting
+    set -x LESS_TERMCAP_se (set_color normal)  # End standout-mode 
+    set -x LESS_TERMCAP_so (set_color yellow)  # Begin standout-mode - info box 
+    set -x LESS_TERMCAP_ue (set_color normal)  # End underline 
+    set -x LESS_TERMCAP_us (set_color --underline red)  # Begin underline
+
+    # ANSI "color" excape sequences are output in "raw" form
+    set -x LESS "-R"
+
+    command man $argv
+end
