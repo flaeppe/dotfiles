08860eb8c4e8d1e74ed6ad0b9352e42d06a9b978
Petter Friberg <petter_friberg@hotmail.com>
2019-07-14 14:16:35 +0200

    Improve workon completion find command
    - Use -path instead of regex
    - Silence permission denied folders
    - Stop descending when match is found

--- fish/.config/fish/completions/workon.fish
+++ fish/.config/fish/completions/workon.fish
@@ -1,1 +1,1 @@
-complete -x -c workon -a "(find $HOME/**repos/ -mindepth 1 -maxdepth 1 -type d | rev | cut -d '/' -f 1 | rev | uniq)"
+complete -x -c workon -a "(find $HOME -mindepth 1 -maxdepth 3 -path '$HOME*repos/*' -prune -type d 2>/dev/null | rev | cut -d '/' -f 1 | rev | uniq)"
