9094c6e040d0b0f51e3ef499e4392f0540d1a7a3
Petter Friberg <petter_friberg@hotmail.com>
2019-07-14 15:01:21 +0200

    Correct workon completion find regex
    - Move optional arguments in front of positional(required) arguments

--- fish/.config/fish/completions/workon.fish
+++ fish/.config/fish/completions/workon.fish
@@ -1,1 +1,1 @@
-complete -x -c workon -a "(find $HOME/*repos/ -type d -mindepth 1 -maxdepth 1 | rev | cut -d '/' -f 1 | rev | uniq)"
+complete -x -c workon -a "(find $HOME/**repos/ -mindepth 1 -maxdepth 1 -type d | rev | cut -d '/' -f 1 | rev | uniq)"
