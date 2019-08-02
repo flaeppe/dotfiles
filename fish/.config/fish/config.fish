544cb7464dad9cccb8e1283707d817cb3c5831e9
Petter Friberg <petter_friberg@hotmail.com>
2019-08-02 08:02:41 +0200

    Set nvim as default editor

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -12,6 +12,8 @@ set -gx LSCOLORS gxfxcxdxbxegedabagacad
 set -gx LS_COLORS 'di=36:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43'
 set -gx CLICOLOR 1
 
+set -x EDITOR nvim
+
 # Set date format language for ls
 set -x LANG en_US.UTF-8
 # TODO: Figure out how this coloring is better controlled by /usr/share/fish/functions/ls.fish
