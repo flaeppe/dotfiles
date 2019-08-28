92bab364253bd725cdbf93e978d0cd6942d04c0e
Petter Friberg <petter_friberg@hotmail.com>
2019-08-29 01:55:56 +0200

    Add 'ripgrep' to .init.sh for Arch
    - Needed for fuzzy finder(fzf) searching

--- fish/.init.sh
+++ fish/.init.sh
@@ -4,7 +4,7 @@ switch (uname)
     case Darwin
             brew install fish fzf ripgrep
     case Linux
-            sudo pacman -S --noconfirm --needed fish fzf
+            sudo pacman -S --noconfirm --needed fish fzf ripgrep
     case '*'
             printf 'ERROR: No installation matching on %s\n' (uname)
             exit 1
