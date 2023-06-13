0f3ad638a5b3935389c30a4345970f9094ca4fa3
Petter Friberg <petter_friberg@hotmail.com>
2023-06-13 22:34:28 +0200

    Clean up the 'fish' directory

--- fish/.config/fish/functions/flog.fish
+++
@@ -1,3 +0,0 @@
-function flog --description 'Fuzzy git log search that outputs the selected hash'
-  git log --color=always --decorate --oneline | fzf --ansi --reverse | awk '{ print $1 }'
-end
--- fish/.init_linux.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env fish
-
-sudo pacman -S --noconfirm --needed fish fzf ripgrep
