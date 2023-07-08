291db39c09a47508b70456335569d7a2ad085561
Petter Friberg <petter_friberg@hotmail.com>
2023-07-08 23:07:07 +0200

    Add editorconfig option

--- home.nix
+++ home.nix
@@ -30,6 +30,26 @@
     file.".gnupg/gpg-agent.conf".source = ./gnupg/gpg-agent.conf;
   };
 
+  editorconfig = {
+    enable = true;
+    settings = {
+      "*" = {
+        charset = "utf-8";
+        end_of_line = "lf";
+        indent_size = 2;
+        trim_trailing_whitespace = true;
+        insert_final_newline = true;
+        indent_style = "space";
+      };
+      "Makefile" = {
+        indent_style = "tab";
+      };
+      "*.py" = {
+        indent_size = 4;
+      };
+    };
+  };
+
   imports = [ ./git.nix ./fish/fish.nix ./nvim/nvim.nix ];
 
   programs = {
