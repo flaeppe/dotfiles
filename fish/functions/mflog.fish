bfbd5f5d636aeeb8e3a54d49e04648e195fb2952
Petter Friberg <3703560+flaeppe@users.noreply.github.com>
2025-08-08 11:10:25 +0200

    Remove fish mflog function

--- fish/functions/mflog.fish
+++
@@ -1,4 +0,0 @@
-# TODO: Replace flog with this
-function mflog --description 'Fuzzy git log search that outputs hashes for selected commits'
-    git log --color=always --decorate --oneline | fzf --ansi --reverse --multi | awk '{ print $1 }' | tr '\n' ' '
-end
