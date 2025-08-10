5f2302aa462e6c0f1a0e4c421a608150177c0311
Petter Friberg <3703560+flaeppe@users.noreply.github.com>
2025-08-10 17:07:40 +0200

    Lua type hint fixes

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,22 +1,24 @@
+local bufopts = { noremap = true }
 --- Navigation
 local kitty_nav = require("plugins/kitty_nav")
 vim.keymap.set("n", "<C-h>", function()
     kitty_nav.navigate("h")
-end)
+end, bufopts)
 vim.keymap.set("n", "<C-j>", function()
     kitty_nav.navigate("j")
-end)
+end, bufopts)
 vim.keymap.set("n", "<C-k>", function()
     kitty_nav.navigate("k")
-end)
+end, bufopts)
 vim.keymap.set("n", "<C-l>", function()
     kitty_nav.navigate("l")
-end)
+end, bufopts)
 --- Quicklist mappings
-vim.keymap.set("n", "<Leader>q", "<Cmd>copen<CR>")
-vim.keymap.set("n", "<Leader>Q", "<Cmd>cclose<CR>")
-vim.keymap.set("n", "<Leader>qj", "<Cmd>try | cnext | catch | cfirst | catch | endtry<CR>")
-vim.keymap.set("n", "<Leader>qk", "<Cmd>try | cprevious | catch | clast | catch | endtry<CR>")
+vim.keymap.set("n", "<Leader>q", "<Cmd>copen<CR>", bufopts)
+vim.keymap.set("n", "<Leader>Q", "<Cmd>cclose<CR>", bufopts)
+vim.keymap.set("n", "<Leader>qj", "<Cmd>try | cnext | catch | cfirst | catch | endtry<CR>", bufopts)
+vim.keymap.set("n", "<Leader>qk", "<Cmd>try | cprevious | catch | clast | catch | endtry<CR>", bufopts)
+
 --- Format buffer
 vim.keymap.set("n", "<Leader>ll", function()
     vim.lsp.buf.format({ async = true })
@@ -32,6 +34,7 @@ end, bufopts)
 -- Show/hide diagnostic
 vim.keymap.set("n", "<Leader>ts", vim.diagnostic.show, bufopts)
 vim.keymap.set("n", "<Leader>th", vim.diagnostic.hide, bufopts)
--- Move to prev/next item
-vim.keymap.set("n", "åd", vim.diagnostic.goto_prev, bufopts)
-vim.keymap.set("n", "¨d", vim.diagnostic.goto_next, bufopts)
+-- Move to next item
+vim.keymap.set("n", "¨d", function() vim.diagnostic.jump({ count = 1, float = true }) end, bufopts)
+-- Move to previous item
+vim.keymap.set("n", "åd", function() vim.diagnostic.jump({ count = -1, float = true }) end, bufopts)
