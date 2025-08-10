35557779cb1c84e65851cb0854005fe793151b0f
Petter Friberg <3703560+flaeppe@users.noreply.github.com>
2025-08-10 21:46:46 +0200

    Remove subshell from CTRL-D commands in gb
    fzf should already have started a new subshell to run the execute hook

--- fish/functions/git-branches.fish
+++ fish/functions/git-branches.fish
@@ -21,28 +21,25 @@ function gb --description "Fuzzy find Git branches with advanced preview and act
         --expect enter \
         --bind "ctrl-d:execute(
             # Extract the branch name from the fzf highlighted line
-            set -l branch_to_delete (echo {} | sed 's/^\* //' | awk '{print \$1}');
-            # Use fish -c to run the confirmation and deletion command
-            fish -c '
-                set -l branch \"\$argv[1]\";
-                read -P \"Delete branch \$branch? (y/N) \" confirm;
+            set -l branch (echo {} | sed 's/^\* //' | awk '{print \$1}');
+            # Require confirmation before deleting
+            read -P \"Delete branch \$branch? (y/N) \" confirm;
 
-                if test \"\$confirm\" = y;
-                    # Prevent deletion of 'main' or 'master' branches
-                    if test \"\$branch\" = \"main\" -o \"\$branch\" = \"master\"
-                        echo \"Error: Cannot delete the \"\$branch\" branch. Deleting \"main\" or \"master\" is not allowed.\"
+            if test \"\$confirm\" = y;
+                # Prevent deletion of 'main' or 'master' branches
+                if test \"\$branch\" = \"main\" -o \"\$branch\" = \"master\"
+                    echo \"Error: Cannot delete the '\$branch' branch. Deleting 'main' or 'master' is not allowed.\"
+                    echo \"Press any key to continue...\"
+                    read -n 1 # Wait for any key press
+                else
+                    if not git branch -D \"\$branch\";
+                        echo \"Error: Failed to delete branch \$branch.\"
                         echo \"Press any key to continue...\"
                         read -n 1 # Wait for any key press
-                    else
-                        if not git branch -D \"\$branch\";
-                            echo \"Error: Failed to delete branch \$branch.\"
-                            echo \"Press any key to continue...\"
-                            read -n 1 # Wait for any key press
-                        end
                     end
                 end
-            ' \"\$branch_to_delete\"
-        )+reload(fish -c 'source ~/.config/fish/functions/gb.fish; __gb_list_branches')+refresh-preview" \
+            end
+        )+reload(source ~/.config/fish/functions/gb.fish; __gb_list_branches)+refresh-preview" \
         --preview-window 'down,border-top,40%' \
         --preview-border line \
         --preview '
